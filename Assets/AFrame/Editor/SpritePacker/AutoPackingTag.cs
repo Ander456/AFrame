886420bb4cae6c7037f8764e2cd03e9ec46bdbda
mio <mio@miodeMacBook-Pro.local>
2020-02-21 19:59:59 +0800

    add sprite packer editor scripts

---
+++ Assets/AFrame/Editor/SpritePacker/AutoPackingTag.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+
+public class AutoPackingTag : AssetPostprocessor 
+{
+	void OnPostprocessTexture (Texture2D texture) 
+	{
+		string AtlasName =  new DirectoryInfo(Path.GetDirectoryName(assetPath)).Name;
+		TextureImporter textureImporter  = assetImporter as TextureImporter;
+		textureImporter.textureType = TextureImporterType.Sprite;
+		textureImporter.spritePackingTag = AtlasName;
+		textureImporter.mipmapEnabled = false;
+	}
+}
