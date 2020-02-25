a96bdb3764b5ebb7fa11768931e7185d949db753
mio <>
2020-02-25 21:28:28 +0800

    modify xcodepostprocess

--- Assets/AFrame/Editor/XcodePostProcess.cs
+++ Assets/AFrame/Editor/XcodePostProcess.cs
@@ -4,6 +4,7 @@ using UnityEditor.iOS.Xcode;
 using System;
 using System.IO;
 using System.Collections.Generic;
+using UnityEngine;
 
 /// https://blog.csdn.net/linxinfa/article/details/87618408
 /// <summary>
@@ -23,39 +24,53 @@ public class XcodePostProcess
 		var projectPath = pathToBuiltProject + "/Unity-iPhone.xcodeproj/project.pbxproj";
 		PBXProject project = new PBXProject();
 		project.ReadFromFile(projectPath);
+
 		string targetGuid = project.TargetGuidByName("Unity-iPhone");
 
 		// 修改BITCODE设置的例子
 		project.SetBuildProperty(targetGuid, "ENABLE_BITCODE", "NO");
-
-		// 你可能会设置的--------------------------------------------------
-		//project.SetBuildProperty(targetGuid, "CODE_SIGN_IDENTITY", "p12证书的code_sign");
-		// 把证书设置设置为手动，即不使用Automatically manage signing
-		//project.SetTargetAttributes("ProvisioningStyle","Manual");
-
-		//project.SetBuildProperty(targetGuid, "PROVISIONING_PROFILE", "mobileprovison文件的UUID");
-		//project.SetBuildProperty(targetGuid, "PROVISIONING_PROFILE_SPECIFIER", "mobileprovison文件的Name");
-
-		//project.SetTeamId(targetGuid,"证书的TeamId");
-		//project.SetBuildProperty(targetGuid, "IPHONEOS_DEPLOYMENT_TARGET", "8.0");
+		// 设置签名证书
+//		project.SetBuildProperty(targetGuid, "CODE_SIGN_IDENTITY", "p12证书的code_sign");
+//		project.SetBuildProperty(targetGuid, "PROVISIONING_PROFILE", "mobileprovison文件的UUID");
+//		project.SetBuildProperty(targetGuid, "PROVISIONING_PROFILE_SPECIFIER", "mobileprovison文件的Name");
+//		project.SetBuildProperty(targetGuid, "DEVELOPMENT_TEAM", "xxx");
+		project.SetBuildProperty(targetGuid, "IPHONEOS_DEPLOYMENT_TARGET", "8.0");
 
 		// 添加framework
-		//project.AddFrameworkToProject(targetGuid, "StoreKit.framework", true);
-		//----------------------------------------------------------------
+		project.AddFrameworkToProject(targetGuid, "StoreKit.framework", true);
+
+		// 添加flag
+//		project.AddBuildProperty(targetGuid, "OTHER_LDFLAGS", "-ObjC");
 
 		// 修改后的内容写回到配置文件
-//		File.WriteAllText(projectPath, project.WriteToString());
-//
-//		// 修改Info.plist的示例
-//		var plistPath = Path.Combine(pathToBuiltProject, "Info.plist");
-//		var plist = new PlistDocument();
-//		plist.ReadFromFile(plistPath);
-//
-//		// 增加字符串类型的设置
-//		plist.root.SetString("fieldname", "value");
-//
-//		// 修改后的内容写回到文件Info.plist
-//		plist.WriteToFile(plistPath);
+		File.WriteAllText(projectPath, project.WriteToString());
+
+		// 修改Info.plist文件
+		var plistPath = Path.Combine(pathToBuiltProject, "Info.plist");
+		var plist = new PlistDocument();
+		plist.ReadFromFile(plistPath);
+
+		PlistElementDict rootDict = plist.root;
+		// 权限
+		rootDict.SetString("NSCameraUsageDescription", "请允许打开摄像头");
+		rootDict.SetString("NSMicrophoneUsageDescription", "请允许打开麦克风");
+
+		// 允许http
+		var atsKey = "NSAppTransportSecurity";
+		PlistElementDict dictTmp = rootDict.CreateDict(atsKey);
+		dictTmp.SetBoolean("NSAllowsArbitraryLoads", true);
+
+		// 插入URL Scheme到Info.plsit（理清结构）
+		var array = rootDict.CreateArray("CFBundleURLTypes");
+		//插入dict
+		var urlDict = array.AddDict();
+		urlDict.SetString("CFBundleTypeRole", "Editor");
+		//插入array
+		var urlInnerArray = urlDict.CreateArray("CFBundleURLSchemes");
+		urlInnerArray.AddString("blablabla");
+	
+		// 应用修改
+		plist.WriteToFile(plistPath);
 
 	}
 }
@@ -159,5 +174,18 @@ public class XcodeExportPlist
 		}
 		sb.Append("\n").Append(ind);
 		sb.Append("</dict>\n");
+	}
+
+	public static string GenArchiveOptPlist()
+	{
+		var path = Application.dataPath.Replace("Assets", "Bin/exportOption.plist");
+		var teamId = "BQCHO456";
+		// "app-store","enterprise","ad-hoc","development"
+		var archiveMethod = "enterprise";
+		var bundleId = "com.linxinfa.test";
+		var provisionName = "linxinfaEnt2019614";
+
+		GenFile(path, teamId, archiveMethod, bundleId, provisionName);
+		return path;
 	}
 }
